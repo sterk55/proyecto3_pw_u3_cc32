1550d02effc87ce28d69843bd1a52351e1ed12f4
sterk55 <Daniel49571@gmail.com>
2023-02-11 04:08:29 -0500

    Tarea16

--- src/main/java/com/uce/edu/pw/proyecto3_pw_u3_cc3/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/uce/edu/pw/proyecto3_pw_u3_cc3/controller/EstudianteControllerRestFul.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,6 +34,7 @@ public class EstudianteControllerRestFul {
     }
 
     @GetMapping(path = "/{id}")
+    @CrossOrigin(origins = "http://localhost:8080")
     public ResponseEntity<Estudiante> encontrarR(@PathVariable("id") Integer id) {
         HttpHeaders cabeceras = new HttpHeaders();
         cabeceras.add("detalleMensaje", "Estudiante econtrado correctamente");
